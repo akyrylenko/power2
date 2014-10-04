5d080ca32ab882bc43268b1cffdfafa56bfe4661
Andriy Kyrylenko <andriy.kyrylenko@gmail.com>
2014-10-04 13:41:35 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Power2::Application.config.secret_token = 'c35bb433008a1f042a2f64722ebd2f3cf5f6654e97dd2c838cf955512e402e2b1cadaa19a8e9097aaffdeeeb5efbddf641c141cfa0ae9a43b1a0a2331ee267f3'
